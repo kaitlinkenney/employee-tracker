1abebf05351f382dd4aaf96d428b4aecf46c5689
kaitlinkenney <kaitlin.kenney96@gmail.com>
2020-11-23 18:16:51 -0500

    addemployee questions running

--- employees.js
+++ employees.js
@@ -33,124 +33,65 @@ function start() {
         choices: ["View all employees", "Update employee role", "Add employee"]
     })
     .then(function (answer) {
-      // based on their answer, either call the bid or the post functions
-      // if (answer.navigate === "View all employees") {
-      //   viewAllEmployees();
-      // }
-      if (answer.nagivate === "Update employee role") {
-        updateRole();
+      if (answer.navigate === "View all employees") {
+        viewAllEmployees();
       }
-      // else{
-      //   addEmployee();
-      // } 
+      // else if (answer.nagivate === "Update employee role") {
+      //   updateRole();
+      // }
+      else{
+        addEmployee();
+      } 
     });
 }
 
-// function viewAllEmployees() {
-//   connection.query(`SELECT
-//   employee.first_name,
-//   employee.last_name,
-//   role.title,
-//   role.salary,
-//   department.name
-//   FROM employee
-//   INNER JOIN role ON role.id = employee.role_id;
-//   INNER JOIN department ON department.id = role.department_id;`, function (err, res) {
-//     if (err) throw err;
-//     for (let i = 0; i < res.length; i++) {
-//       console.log(res[i].first_name + " | " + res[i].last_name + " | " + res[i].tile + "|" + res[i].salary + "|" + res[i].name);
-//       returnHome();
-//     }
-//   });
-// }
-
-function updateRole() {
-  inquirer
-    .prompt([
-      {
-        name: "udpateName",
-        type: "input",
-        message: "What is the employee's last name?"
-      },
-      {
-        name: "updateRole",
-        type: "input",
-        message: "What is the employee's updated role?"
-      },
-    ])
-    .then(function (answer) {
-      connection.query(
-        "UPDATE roles SET ? WHERE ?",
-        [
-          {
-            title: answer.updateRole
-          },
-          {
-            last_name: answer.updateName
-          }
-        ],
-        function (err, res) {
-          if (err) throw err;
-          console.log(res.affectedRows + "employee(s) updated.\n");
-          returnHome();
-        }
-      )
-    });
-  console.log(query.sql);
+function viewAllEmployees() {
+  connection.query(`SELECT
+  employee.first_name,
+  employee.last_name,
+  role.title,
+  role.salary,
+  department.name
+  FROM employee
+  INNER JOIN role ON role.id = employee.role_id
+  INNER JOIN department ON department.id = role.department_id`,
+      function (err, res) {
+    if (err) throw err;
+    for (let i = 0; i < res.length; i++) {
+      console.log(res[i].first_name + " | " + res[i].last_name + " | " + res[i].tile + "|" + res[i].salary + "|" + res[i].name);
+      returnHome();
+    }
+  });
 }
 
-// function addEmployee() {
+// function updateRole() {
 //   inquirer
 //     .prompt([
 //       {
-//         name: "addFirst",
+//         name: "udpateName",
 //         type: "input",
-//         message: "Enter the employee's first name:"
+//         message: "What is the employee's last name?"
 //       },
 //       {
-//         name: "addLast",
+//         name: "updateRole",
 //         type: "input",
-//         message: "Enter the employee's last name:"
+//         message: "What is the employee's updated role?"
 //       },
-//       {
-//         name: "addDept",
-//         type: "input",
-//         message: "Enter the employee's department name:"
-//       },
-//       {
-//         name: "addTitle",
-//         type: "input",
-//         message: "Enter the employee's job title:"
-//       },
-//       {
-//         name: "addSalary",
-//         type: "input",
-//         message: "Enter the employee's salary:"
-//       }
 //     ])
 //     .then(function (answer) {
-//       var query = connection.query(
-//         "INSERT INTO department SET ?",
+//       connection.query(
+//         "UPDATE roles SET ? WHERE ?",
 //         [
 //           {
-//             first_name: answer.addFirst
+//             title: answer.updateRole
 //           },
 //           {
-//             last_name: answer.addLast
-//           },
-//           {
-//             name: answer.addDept
-//           },
-//           {
-//             title: answer.addTitle
-//           },
-//           {
-//             salary: answer.addSalary
+//             last_name: answer.updateName
 //           }
 //         ],
 //         function (err, res) {
 //           if (err) throw err;
-//           console.log(res.affectedRows + " employee added!\n");
+//           console.log(res.affectedRows + "employee(s) updated.\n");
 //           returnHome();
 //         }
 //       )
@@ -158,21 +99,78 @@ function updateRole() {
 //   console.log(query.sql);
 // }
 
-function returnHome() {
+function addEmployee() {
   inquirer
     .prompt([
       {
-        name: "home",
-        type: "list",
-        message: "Would you like to return home?",
-        choices: ["Yes", "No"]
+        name: "addFirst",
+        type: "input",
+        message: "Enter the employee's first name:"
       },
+      {
+        name: "addLast",
+        type: "input",
+        message: "Enter the employee's last name:"
+      },
+      {
+        name: "addDept",
+        type: "input",
+        message: "Enter the employee's department name:"
+      },
+      {
+        name: "addTitle",
+        type: "input",
+        message: "Enter the employee's job title:"
+      },
+      {
+        name: "addSalary",
+        type: "input",
+        message: "Enter the employee's salary:"
+      }
     ])
     .then(function (answer) {
-      if (answer.home === "Yes") {
-        start();
-      } else {
-        connection.end();
-      }
-    })
+      var query = connection.query(
+        "INSERT INTO department SET ?",
+        [
+          {
+            first_name: answer.addFirst
+          },
+          {
+            last_name: answer.addLast
+          },
+          {
+            name: answer.addDept
+          },
+          {
+            title: answer.addTitle
+          },
+          {
+            salary: answer.addSalary
+          }
+        ],
+        function (err, res) {
+          if (err) throw err;
+          console.log(res.affectedRows + " employee added!\n");
+          // returnHome();
+        }
+      )
+      console.log(query.sql);
+    });
 }
+
+// function returnHome() {
+//   inquirer
+//     .prompt({
+//         name: "home",
+//         type: "list",
+//         message: "Would you like to return home?",
+//         choices: ["Yes", "No"]
+//     })
+//     .then(function (answer) {
+//       if (answer.home === "Yes") {
+//         start();
+//       } else {
+//         connection.end();
+//       }
+//     })
+// }
